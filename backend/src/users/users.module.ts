7c57b1ce56776dc53d4fd875dc44ea7d328a5773
MatiasGEscobar <matiie9135@gmail.com>
2026-03-27 01:42:26 -0300

    configuracion users (a modificar)

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/users.entity';
+import { requiresAuth } from 'express-openid-connect';
+import { RolesModule } from 'src/roles/roles.module';
+import { FileUploadModule } from 'src/file-upload/file-upload.module';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([User]), RolesModule, FileUploadModule],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(requiresAuth).forRoutes('users/profile');
+  }
+}
